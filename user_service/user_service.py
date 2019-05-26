6f6cb3265d6cd13f62ff0cc4b18446fd03e25e00
AdoreIt <NatalieLytvyniuk@gmail.com>
2019-05-26 16:09:19 +0300

    i #44: Implement on check UserService responce

--- user_service/user_service.py
+++ user_service/user_service.py
@@ -6,24 +6,57 @@ app = Flask(__name__)
 api = Api(app)
 
 
+def get_users_lockers_dict():
+    return {"Ann": 1, "Olha": 2, "Natalie": None, "Alice": 3}
+
+
+def get_users_locker(user_name):
+    users_lockers = get_users_lockers_dict()
+    if user_name in users_lockers:
+        locker = users_lockers[user_name]
+        if locker is not None:
+            return locker
+        else:
+            return "user without locker"
+    else:
+        return "user not exists"
+
+
+class Users(Resource):
+    def get(self):
+        print("Users: get")
+        try:
+            print("Users: trying to get users")
+            users = get_users_lockers_dict()
+            users_answer = {'users': users}
+
+            print("Users: sending answer")
+            return users_answer, 404
+        except:
+            print("Users: troubles")
+            return 'Cannot connect to users db', 404
+
+
 class UserService(Resource):
     def get(self):
-        print("Trying")
+        print("UserService: get")
         try:
-            print("try")
+            print("UserService: trying to get user's locker")
+            user_name_req = request.form['user_name']
             # TODO: Substitute dummy dictionary with DB data
-            answer = {"User Ann": "Locker 1",
-                      "User Olga": "Locker 3",
-                      "User Natalie": "No locker assigned",
-                      "User Alice": "Locker 2"}
-            print(answer)
-            result = {'hello': answer}
+            users_locker = get_users_locker(user_name_req)  #answer
+
+            print("UserService: ", users_locker)
+            result = {'users_locker': users_locker}
+
+            print("UserService: sending answer")
             return result, 200
         except:
-            print("Hello World problems!")
-            return '', 404
+            print("UserService: 'Cannot connect to users db'")
+            return 'Cannot connect to users db', 404
 
 
+api.add_resource(Users, '/users')
 api.add_resource(UserService, '/users_service')
 if __name__ == '__main__':
-    app.run(host="192.168.43.76", port="5010", debug=True)
+    app.run(host="192.168.43.136", port="5010", debug=True)
