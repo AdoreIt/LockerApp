039f2489e94daaf5ae17b5840c748165fc9a05b8
serhiy <serhiylatyuk55@gmail.com>
2019-05-26 14:09:31 +0300

    add lockers table initialization

--- dbs/migrations_mongo/create_lockers_db.js
+++ dbs/migrations_mongo/create_lockers_db.js
@@ -1,2 +1,8 @@
+var MAX_SIZE = 20;
 db = db.getSiblingDB('lockers')
-db.createCollection('lockers', {max: 20})
+db.createCollection('lockers', {capped: true, size: 100000, max: MAX_SIZE})
+
+var i;
+for (i = 0; i < MAX_SIZE; i++) {
+    db.lockers.insert({"_id": i, "free": true})
+}
