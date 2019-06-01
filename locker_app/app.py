07d0c06e9087c6f8940e3200e141cb4cddcccc19
helga-helga <13537448+Helga-Helga@users.noreply.github.com>
2019-06-01 11:18:14 +0300

    i #65: Enable showing of animal logo on each page

--- locker_app/app.py
+++ locker_app/app.py
@@ -1,5 +1,7 @@
-import sys, os
+import os
+import sys
 import json
+import random
 
 import requests
 from flask import Flask, render_template, request, redirect, url_for
@@ -13,9 +15,20 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '2'
 
 
+animal_dict = {"cat": "Furry", "crow": "Black", "dog": "Happy",
+               "dove": "White", "dragon": "Chinese", "fish": "Wet",
+               "frog": "Green", "hippo": "Cute", "horse": "Fast",
+               "kiwi-bird": "Small", "otter": "Cunny", "spider": "Frightening"}
+animal = random.choice(list(animal_dict.keys()))
+animal_class = "fas fa-" + animal
+animal_name = animal_dict[animal] + " " + animal
+
+
 @app.route('/')
 def home():
-    return render_template("home.html")
+    print(animal_class)
+    return render_template("home.html",
+                           animal_class=animal_class, animal_name=animal_name)
 
 
 @app.route('/', methods=['POST'])
@@ -24,45 +37,30 @@ def check():
     no_user_message = None
     no_locker_message = None
     error = None
-    new_locker_message = None
-    no_new_locker = None
 
     name = request.form.get("name", "")
-    resp = requests.get(
-        'http://{0}:{1}/users_service'.format(config.user_service_ip,
-                                              config.user_service_port),
-        data={"user_name": name})
+    try:
+        resp = requests.get(
+            'http://{0}:{1}/users_service'.format(config.user_service_ip,
+                                                  config.user_service_port),
+            data={"user_name": name})
 
-    if resp.status_code == 200:
-        resp = json.loads(resp.text)
-        locker_answer = resp["users_locker"]
+        if resp.status_code == 200:
+            resp = json.loads(resp.text)
+            locker_answer = resp["users_locker"]
 
-        if locker_answer == "user not exists":
-            no_user_message = "User {} doesn't exist".format(name)
-        if locker_answer == "user without locker":
-            no_locker_message = "User {} has no locker".format(name)
-            resp = requests.get(
-                'http://{0}:{1}/users_service'
-                .format(config.user_service_ip,
-                        config.user_service_port),
-                data={"user_name": name})
-            if resp.status_code == 200:
-                resp = json.loads(resp.text)
-                locker_answer = resp["users_locker"]
-                print("LOCKER ANSWER", locker_answer)
-                if locker_answer == "user without locker":
-                    no_new_locker = "All lockers are busy"
-                elif locker_answer == "locker_db_error":
-                    no_new_locker = "Locker DB error: read only"
-                else:
-                    new_locker_message = "User {} occupies locker {}".format(
-                        name, locker_answer)
-            elif resp.status_code == 404:
-                error = resp.text
-        else:
-            message = "User {} occupies locker {}".format(name, locker_answer)
-    elif resp.status_code == 404:
-        error = resp.text
+            if locker_answer == "user not exists":
+                no_user_message = "User {} doesn't exist".format(name)
+            if locker_answer == "user without locker":
+                no_locker_message = "User {} has no locker".format(name)
+            else:
+                message = "User {} occupies locker {}".format(name,
+                                                              locker_answer)
+        elif resp.status_code == 404:
+            error = resp.text
+
+    except:
+        error = "Service temporary anavailable"
 
     return render_template(
         "check.html",
@@ -70,8 +68,7 @@ def check():
         info=no_locker_message,
         warning=no_user_message,
         error=error,
-        new_locker_message=new_locker_message,
-        no_new_locker=no_new_locker)
+        animal_class=animal_class, animal_name=animal_name)
 
 
 @app.route('/users', methods=['GET'])
@@ -83,18 +80,25 @@ def users():
         resp = requests.get('http://{0}:{1}/users'.format(
             config.user_service_ip, config.user_service_port))
         print("LockerApp got response: " + resp.text)
+
+        if resp.status_code == 200:
+            print("LockerApp: Loading response from UserService")
+            resp = json.loads(resp.text)
+            data = resp["users"]
+            return render_template("users.html", data=data,
+                                   animal_class=animal_class,
+                                   animal_name=animal_name)
+        else:
+            error = resp.text
+            return render_template("users.html", error=error,
+                                   animal_class=animal_class,
+                                   animal_name=animal_name)
+
     except:
         error = "Service temporary unavailable. Please, try later"
-        return render_template("users.html", error=error)
-
-    if resp.status_code == 200:
-        print("LockerApp: Loading response from UserService")
-        resp = json.loads(resp.text)
-        data = resp["users"]
-        return render_template("users.html", data=data)
-    else:
-        error = "No Hello World for you >:|"
-        return render_template("users.html", error=error)
+        return render_template("users.html", error=error,
+                               animal_class=animal_class,
+                               animal_name=animal_name)
 
 
 @app.route('/lockers', methods=['GET'])
@@ -108,19 +112,25 @@ def lockers():
                                                    config.locker_service_port),
             data={'lockers': "LockerService"})
         print("LockerApp got response: " + resp.text)
+
+        if resp.status_code == 200:
+            print("LockerApp: Loading response from LockerService")
+            resp = json.loads(resp.text)
+            data = resp['lockers']
+            return render_template("lockers.html", data=data,
+                                   animal_class=animal_class,
+                                   animal_name=animal_name)
+        else:
+            error = resp.text
+            return render_template("lockers.html", error=error,
+                                   animal_class=animal_class,
+                                   animal_name=animal_name)
+
     except:
         error = "Service temporary unavailable. Please, try later"
-        return render_template("lockers.html", error=error)
-
-    if resp.status_code == 200:
-        print("LockerApp: Loading response from LockerService")
-        resp = json.loads(resp.text)
-        data = resp['lockers']
-        return render_template("lockers.html", data=data)
-    else:
-        error = "No Hello World for you >:|"
-        return render_template("lockers.html", error=error)
-
+        return render_template("lockers.html", error=error,
+                               animal_class=animal_class,
+                               animal_name=animal_name)
 
 if __name__ == "__main__":
     app.run(host=config.locker_app_ip, port=config.locker_app_port, debug=True)
